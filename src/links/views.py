a19b633c33620288671adcbb4a0c558d659c2e3f
rachit98 <rachity@gmail.com>
2021-04-12 13:35:41 +0530

    Add: Added views.py and template

--- src/links/views.py
+++ src/links/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import Link
+from .forms import AddLinkForm
 
 # Create your views here.
+
+def home_view(request):
+    no_discounted = 0
+    error = None
+
+    form = AddLinkForm(request.POST or None)
+
+    if request.method == 'POST':
+        try:
+            if form.is_valid():
+                form.save()
+        except AttributeError:
+            error = "Couldn't get the name or the price"
+        except
+            error = "Something went wrong"
+
+    form = AddLinkForm()
+
+    qs = Link.objects.all()
+    items_no = qs.count()
+
+    if items_no > 0:
+        discount_list = []
+        for item in qs:
+            if item.old_price > item.current_price:
+                discount_list.append(item)
+            no_discounted = len(discount_list)
+
+    context = {
+        'qs':qs,
+        'items_no':items_no,
+        'no_discounted': no_discounted,
+        'form':form,
+        'error':error,
+    }
+
+    return render(request,'links/main.html',context)
